0f7c6773feb54aab5ffaf086cb9425ea1d1b6ecb
e-g-b <ebusch@gannett.com>
2018-01-29 19:28:17 -0500

    hot reloading not working properly, state of count is being reset when JSX changes, however count keeps state with CSS changes

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,8 +1,12 @@
 const path = require("path")
+const webpack = require("webpack")
+const HTMLWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
   entry: {
-    main: "./src/main.js",
+    main: [
+      "./src/main.js"
+    ]
   },
   output: {
     filename: "[name]-bundle.js",
@@ -10,5 +14,64 @@ module.exports = {
   },
   devServer: {
     contentBase: "dist",
-  }
+    overlay: true,
+    hot: true,
+    stats: {
+      colors: true
+    }
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: "babel-loader"
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader"
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: {
+              attrs: ["img:src"]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpg|gif|png)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "images/[name].[ext]"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NamedModulesPlugin(),
+    new HTMLWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
 }
